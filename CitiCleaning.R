76a2c1144ce6a39a5d6fdc5150d2c211f4096ac8
Justin Applefield <jmapplef@gmail.com>
2021-12-03 22:15:41 -0500

    Fix cleaning mor

--- CitiCleaning.R
+++ CitiCleaning.R
@@ -17,7 +17,7 @@ citi$starttime <- as.POSIXct(strptime(citi$starttime, "%Y-%m-%d %H:%M:%S"))
 citi$stoptime <- as.POSIXct(strptime(citi$stoptime, "%Y-%m-%d %H:%M:%S"))
 citi$starthour <- hour(citi$starttime)
 citi$day <- date(citi$starttime)
-citi$month <- as.factor(month(citi$startdate))
+citi$month <- as.factor(month(citi$starttime))
 citi$numWeekday <- wday(citi$starttime)
 citi$dayid <- as.factor(ifelse(citi$numWeekday < 6, "Weekday", "Weekend"))
 citi$weekNum <- week(citi$starttime)
@@ -30,40 +30,58 @@ citi$speed <- citi$distmeters / citi$tripduration
 library(ggplot2)
 citi %>%
   group_by(starthour) %>%
-  summarize(count = n(),
-            dist = mean(distmeters, na.rm = TRUE),
-            dur = mean(tripduration, na.rm = TRUE),
-            speed = dist/dur) %>%
-  ggplot(aes(x=starthour, y = count, fill = count)) + geom_col()
+  summarize(
+    count = n(),
+    dist = mean(distmeters, na.rm = TRUE),
+    dur = mean(tripduration, na.rm = TRUE),
+    speed = dist / dur
+  ) %>%
+  ggplot(aes(x = starthour, y = count, fill = count)) +
+  geom_col()
 
 citi %>%
   group_by(month) %>%
-  summarize(count = n(),
-            dist = mean(distmeters, na.rm = TRUE),
-            dur = mean(tripduration, na.rm = TRUE),
-            speed = dist/dur) %>%
-  ggplot(aes(x=month, y = count, fill = count)) + geom_col()
+  summarize(
+    count = n(),
+    dist = mean(distmeters, na.rm = TRUE),
+    dur = mean(tripduration, na.rm = TRUE),
+    speed = dist / dur
+  ) %>%
+  ggplot(aes(x = month, y = count, fill = count)) +
+  geom_col()
 
 
-#Merging Datasets
+# Merging Datasets
 weather <- read.csv("NYCWeather2019.csv")
 weather$STATION <- NULL
 weather$NAME <- NULL
-weather$DATE <- as.Date(weather$DATE, format= "%m/%d/%Y")
-weather$TAVG <- (weather$TMAX + weather$TMIN)/2
+weather$DATE <- as.Date(weather$DATE, format = "%m/%d/%Y")
+weather$TAVG <- (weather$TMAX + weather$TMIN) / 2
 
 
 
 citiday <- citi %>%
   group_by(day, gender, dayid) %>%
-  summarize(count = n(),
-            dist = mean(distmeters, na.rm = TRUE),
-            dur = mean(tripduration, na.rm = TRUE),
-            speed = dist/dur)
+  summarize(
+    count = n(),
+    dist = mean(distmeters, na.rm = TRUE),
+    dur = mean(tripduration, na.rm = TRUE),
+    speed = dist / dur
+  )
 
 combined_gender <- merge(citiday, weather, by.x = "day", by.y = "DATE")
 
-ggplot(combined_gender, aes(x = TAVG, y = count)) + geom_point(size = 2, alpha = 0.5, color = "yellowgreen") + geom_smooth(color = "darkgreen") + facet_wrap( ~ gender)
-ggplot(combined_gender, aes(x = TAVG, y = count)) + geom_point(size = 2, alpha = 0.5, color = "yellowgreen") + geom_smooth(color = "darkgreen") + facet_wrap( ~ dayid)
-ggplot(combined_gender, aes(x = TAVG, y = dist)) + geom_point(size = 2, alpha = 0.5, color = "yellowgreen") + geom_smooth(color = "darkgreen") 
-ggplot(combined_gender, aes(x = TAVG, y = speed)) + geom_point(size = 2, alpha = 0.5, color = "yellowgreen") + geom_smooth(color = "darkgreen") 
+ggplot(combined_gender, aes(x = TAVG, y = count)) +
+  geom_point(size = 2, alpha = 0.5, color = "yellowgreen") +
+  geom_smooth(color = "darkgreen") +
+  facet_wrap(~gender)
+ggplot(combined_gender, aes(x = TAVG, y = count)) +
+  geom_point(size = 2, alpha = 0.5, color = "yellowgreen") +
+  geom_smooth(color = "darkgreen") +
+  facet_wrap(~dayid)
+ggplot(combined_gender, aes(x = TAVG, y = dist)) +
+  geom_point(size = 2, alpha = 0.5, color = "yellowgreen") +
+  geom_smooth(color = "darkgreen")
+ggplot(combined_gender, aes(x = TAVG, y = speed)) +
+  geom_point(size = 2, alpha = 0.5, color = "yellowgreen") +
+  geom_smooth(color = "darkgreen")
