9fbbb8e64cc2fc38a11b864225a01c48bc95ed41
Justin Applefield <jmapplef@gmail.com>
2021-12-07 12:43:50 -0500

    Progress on bike usage

--- Cleaning_Justin.R
+++ Cleaning_Justin.R
@@ -3,41 +3,30 @@ library(janitor)
 library(lubridate)
 library(geosphere)
 
-bike <-
-    list.files(pattern = "*citibike*") %>% 
-    map_df(~read_csv(., col_types = cols('start station id'='d', 'end station id'='d')))
+citibike <-
+    list.files(pattern = "*citibike*") %>%
+    map_df(~ read_csv(.))
 
-bike <- clean_names(bike)
-bike$usertype <- as.factor(bike$usertype)
-bike$gender <- ifelse(bike$gender == 1, "male", ifelse(bike$gender == 2, "female", "unknown"))
-bike$gender <- as.factor(bike$gender)
+citibike <- clean_names(citibike)
 
-samplefrac <- 0.01
-bike <- sample_frac(bike, samplefrac)
+citibike$X <- NULL
+citibike$start_station_id <- as.factor(citibike$start_station_id)
+citibike$end_station_id <- as.factor(citibike$end_station_id)
+citibike$bikeid <- as.factor(citibike$bikeid)
+citibike$usertype <- as.factor(citibike$usertype)
+# Fix gender
+citibike$gender <- ifelse(citibike$gender == 1, "male", ifelse(citibike$gender == 2, "female", "unknown"))
+citibike$gender <- as.factor(citibike$gender)
+# Create a column for approximate age
+citibike$age <- 2021 - citibike$birth_year
 
-bike$startDate <- as.Date(bike$starttime)
-bike$stopDate <- as.Date(bike$stoptime)
-bike$startMonth <- month(bike$starttime)
-bike$stopMonth <- month(bike$stopDate)
-bike$startMonthFactor <- as.factor(month(bike$startDate))
-bike$stopMonthFactor <- as.factor(month(bike$stopDate))
-bike$numWeekday <- as.factor(wday(bike$startDate))
-bike$weekNum <- as.numeric(strftime(bike$startDate, format = "%V"))
 
-bike$distMeters <- distHaversine(cbind(bike$start_station_latitude, bike$start_station_longitude), cbind(bike$end_station_latitude, bike$end_station_longitude))
+citibike$starthour <- hour(citibike$starttime)
+citibike$day <- date(citibike$starttime)
+citibike$month <- as.factor(month(citibike$starttime))
+citibike$numWeekday <- wday(citibike$starttime)
+citibike$dayid <- as.factor(ifelse(citibike$numWeekday < 6, "Weekday", "Weekend"))
+citibike$weekNum <- week(citibike$starttime)
 
-bike$speedMetersperSec <- bike$distMeters / bike$tripduration
-bike$age <- 2020 - bike$birth_year
-
-bike$start.station.id <- as.factor(bike$start_station_id)
-bike$end.station.id <- as.factor(bike$end_station_id)
-bike$bikeid <- as.factor(bike$bikeid)
-
-bike$day <- as.Date(bike$starttime)
-bike$dayid <- as.factor(ifelse(as.numeric(bike$numWeekday) < 6, "Weekday", "Weekend"))
-
-weather <- read_csv("NYCWeather2019.csv")
-weather$STATION <- NULL
-weather$NAME <- NULL
-weather$DATE <- as.Date(weather$DATE, format="%m/%d/%Y")
-weather$TAVG <- (weather$TMAX + weather$TMIN) / 2
+citibike$distmeters <- distHaversine(cbind(citibike$start_station_latitude, citibike$start_station_longitude), cbind(citibike$end_station_latitude, citibike$end_station_longitude))
+citibike$speed <- citibike$distmeters / citibike$tripduration
